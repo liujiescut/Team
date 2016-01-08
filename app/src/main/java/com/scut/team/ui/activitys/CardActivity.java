8c150749510b00ea9f8904d4efd30ec0676ed1b5
GaoZhen <912815282@qq.com>
2016-01-08 22:55:40 +0800

    添加acitvity，基本类齐了，有的Activity是空的

---
+++ app/src/main/java/com/scut/team/ui/activitys/CardActivity.java
@@ -0,0 +1,14 @@
+package com.scut.team.ui.activitys;
+
+import com.scut.team.ui.base.BasePageActivity;
+
+/**
+ * create for the detail of Card
+ * Created by host on 2016/1/8.
+ */
+public class CardActivity extends BasePageActivity {
+    @Override
+    protected void setLayoutView() {
+
+    }
+}
---
+++ app/src/main/java/com/scut/team/ui/fragments/CardListFragment.java
@@ -0,0 +1,10 @@
+package com.scut.team.ui.fragments;
+
+import com.scut.team.ui.base.BaseFragment;
+
+/**
+ * create for wrap list
+ * Created by host on 2016/1/8.
+ */
+public class CardListFragment extends BaseFragment{
+}
