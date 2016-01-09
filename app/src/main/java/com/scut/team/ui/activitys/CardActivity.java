c7f3c379101aadd231ff9e6f7fe48d7529e7bd3c
GaoZhen <912815282@qq.com>
2016-01-10 01:02:34 +0800

    cardActivity MainActivity

--- app/src/main/java/com/scut/team/ui/activitys/CardActivity.java
+++ app/src/main/java/com/scut/team/ui/activitys/CardActivity.java
@@ -1,14 +1,139 @@
 package com.scut.team.ui.activitys;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.scut.team.R;
+import com.scut.team.model.Card;
+import com.scut.team.model.User;
 import com.scut.team.ui.base.BasePageActivity;
+
+import java.util.ArrayList;
 
 /**
  * create for the detail of Card
  * Created by host on 2016/1/8.
  */
 public class CardActivity extends BasePageActivity {
+    private PopupWindow mFunctionPopupWindow;
+    private TextView mBoardNameTextView;
+    private Fragment mListFragment;
+    private TextView mTitleTextView;
+    private RelativeLayout mTitleRelativieLayout;
+    private ArrayList mCardList;
+
     @Override
     protected void setLayoutView() {
+        setContentView(R.layout.activity_board);
+    }
+
+    protected void findViews() {
+        User user = DBhelper.getInstance().getFirstUser();
+        Card selectCard = new Card();
+        selectCard.setDueDate(DueDate);
+        Cards.clear();
+        for (Card t : user.getCards()) {
+            if (t.getDueDateString().equals(selectCard.getDueDateString())) {
+                Cards.add(t);
+            }
+        }
+    }
+
+    protected void setupViews(Bundle bundle) {
+        convertView = inflater.inflate(R.layout.fragment_calendar, container, false);
+        CardView = findView(R.id.calendar_view);
+    }
+
+    protected void setListener() {
+        CardView.setOnDueDateSelectedListener(new CardView.OnDueDateSelectedListener() {
+            @Override
+            public void onDueDateSelected(DueDate DueDate) {
+                initView();
+            }
+
+            @Override
+            public void onDueDateUnselected(DueDate DueDate) {
+
+            }
+        });
+    }
+
+    protected void fetchData() {
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Bundle bundle = new Bundle();
+                bundle.putSerializable(Constant.DataKey.card, cards.get(position));
+                Intent intent = new Intent(getActivity(), cardActivity.class);
+                intent.putExtras(bundle);
+                startActivityForResult(intent, Constant.Code.MODIFY_card);
+            }
+        });
 
     }
+
+    protected void renameBoard(String newBoardName) {
+        //设置长按删除
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+                String[] items = {"删除"};
+                AlertDialog alertDialog = new AlertDialog.Builder(getContext())
+                        .setItems(items, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                //远程删除
+                                remoteDelete(cards.get(position).getId());
+                                //数据库中删除
+                                DBhelper.getInstance().delete(cards.get(position));
+                                Activity mainActivity = getActivity();
+                                if(mainActivity instanceof MainActivity){
+                                    ((MainActivity)mainActivity).fragmentInitView();
+                                }
+                            }
+                        })
+                        .create();
+                alertDialog.show();
+                return true;
+            }
+        });
+        setLeftListener();
+    }
+
+    protected void createList(String listName) {
+        //设置长按删除
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+                String[] items = {"删除"};
+                AlertDialog alertDialog = new AlertDialog.Builder(getContext())
+                        .setItems(items, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                //远程增加
+                                remoteAdd(cards.get(position).getId());
+                                //数据库中添加
+                                DBhelper.getInstance().add(cards.get(position));
+                                Activity mainActivity = getActivity();
+                                if(mainActivity instanceof MainActivity){
+                                    ((MainActivity)mainActivity).fragmentInitView();
+                                }
+                            }
+                        })
+                        .create();
+                alertDialog.show();
+                return true;
+            }
+        });
+        setLeftListener();
+    }
+
 }
--- app/src/main/java/com/scut/team/ui/activitys/MainActivity.java
+++ app/src/main/java/com/scut/team/ui/activitys/MainActivity.java
@@ -1,14 +1,26 @@
 package com.scut.team.ui.activitys;
 
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
 import com.scut.team.R;
 import com.scut.team.ui.base.BasePageActivity;
-import com.yydcdut.sdlv.SlideAndDragListView;
+
+import java.util.ArrayList;
 
 /**
  * 程序主界面
  */
 public class MainActivity extends BasePageActivity {
-    private SlideAndDragListView mListView;
+    private PopupWindow mFunctionPopupWindow;
+    private ListView mBoardListView;
+    private TextView mTitleTextView;
+    private RelativeLayout mTitleRelativieLayout;
+    private ArrayList mBoradList;
+
     @Override
     protected void setLayoutView() {
         setContentView(R.layout.activity_main);
@@ -16,6 +28,107 @@ public class MainActivity extends BasePageActivity {
 
     @Override
     protected void findViews() {
-        mListView = (SlideAndDragListView)findViewById(R.id.main_lv_my_board);
+        mBoardListView = (ListView) findViewById(R.id.main_lv_my_board);
     }
+
+    protected void setupViews(Bundle bundle) {
+        mTabWidget = findView(R.id.tabwidget);
+        mViewPager = findView(R.id.viewpager);
+        //设置主界面下方的图标
+        View tab1 = LayoutInflater.from(this).inflate(R.layout.tab1, null);
+        View tab2 = LayoutInflater.from(this).inflate(R.layout.tab2, null);
+        View tab3 = LayoutInflater.from(this).inflate(R.layout.tab3, null);
+        mTabWidget.addTab(tab1);
+        mTabWidget.addTab(tab2);
+        mTabWidget.addTab(tab3);
+        mTabWidget.setDividerInvisible();
+
+        mPagerAdapter = new TabPagerAdapter(getSupportFragmentManager(), fragments);
+        mViewPager.setAdapter(mPagerAdapter);
+        mTabWidget.setmViewPager(mViewPager);
+        mViewPager.setOffscreenPageLimit(2);
+
+        mTabWidget.setmOnTabSelectedListener(new OnTabSelectedListener() {
+            @Override
+            public void onSelected(List<View> tabViews, int position) {
+                switch (position) {
+                    case 0:
+                        aQuery.id(R.id.img_tab_calendar).image(R.mipmap.ic_calendar_grey);
+                        aQuery.id(R.id.img_tab_user).image(R.mipmap.ic_user_grey);
+                        aQuery.id(R.id.img_tab_timetable).image(R.mipmap.ic_timetable);
+                        break;
+                    case 1:
+                        aQuery.id(R.id.img_tab_calendar).image(R.mipmap.ic_calendar);
+                        aQuery.id(R.id.img_tab_user).image(R.mipmap.ic_user_grey);
+                        aQuery.id(R.id.img_tab_timetable).image(R.mipmap.ic_timetable_grey);
+                        break;
+                    case 2:
+                        aQuery.id(R.id.img_tab_calendar).image(R.mipmap.ic_calendar_grey);
+                        aQuery.id(R.id.img_tab_user).image(R.mipmap.ic_user);
+                        aQuery.id(R.id.img_tab_timetable).image(R.mipmap.ic_timetable_grey);
+                        break;
+                }
+            }
+        });
+    }
+
+    protected void setListener() {
+        aQuery.id(R.id.btn_title_left).clicked(this, "onBackPressed");
+        aQuery.id(R.id.btn_title_right2).clicked(this, "createTimetable");
+        aQuery.id(R.id.btn_title_right).clicked(this, "search");
+    }
+
+    protected void fetchData() {
+        Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
+        String timeTableString = gson.toJson(createTimetable);
+        LogUtil.i(timeTableString);
+    }
+
+    protected void createBoard(String boardName) {
+        Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
+        String timeTableString = gson.toJson(createTimetable);
+        LogUtil.i(timeTableString);
+        Map<String, String> param = new HashMap<>();
+        param.put("data", timeTableString);
+        Net.post(Constant.URL.Add, param, new Net.NetworkListener() {
+            @Override
+            public void onSuccess(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    if (jsonObject.optInt("state") == 0) {
+                        toast("添加成功");
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    remoteAdd(createTimetable);
+                }
+            }
+
+            @Override
+            public void onFail(String error) {
+                remoteAdd(createTimetable);
+            }
+        });
+    }
+
+    protected void onBoardClick() {
+    }
+
+    protected void onBoradLongClick() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("确定退出？")
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                })
+                .setPositiveButton("退出", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
+        builder.create().show();
+    }
+
 }
